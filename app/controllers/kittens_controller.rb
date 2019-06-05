93330ec9fa26e8407ded2a7b4077118bf6cad4d9
jamcry <jamcry@hotmail.com>
2019-06-05 12:53:45 +0300

    Complete controller actions and views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,51 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Created kitten!"
+      redirect_to @kitten
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Updated kitten!"
+      redirect_to @kitten
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    if @kitten.destroy
+      flash[:success] = "Deleted kitten."
+      redirect_to kittens_path
+    end
   end
+
+  private
+
+      def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+      end
 end
