41588d7eead5b0982f82eee707532dbdfdcc19bc
jamcry <jamcry@hotmail.com>
2019-06-05 12:21:44 +0300

    Create Kitten model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'kittens/index'
+  get 'kittens/show'
+  get 'kittens/new'
+  get 'kittens/create'
+  get 'kittens/edit'
+  get 'kittens/update'
+  get 'kittens/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
